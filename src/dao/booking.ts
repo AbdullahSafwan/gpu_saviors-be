f6e7931e6b6f8f46db7811cc8390c58fc39e5255
Abdul Qayoom <qayoomk013@gmail.com>
2024-10-28 15:36:48 +0500

    Feature#6 booking-dao

--- src/dao/booking.ts
+++ src/dao/booking.ts
@@ -1,12 +1,15 @@
-// import prisma from "../prisma";
-// import { Prisma } from "@prisma/client";
+import prisma from "../prisma";
+import { Prisma } from "@prisma/client";
 
 
-// const createBooking = async (data: Prisma.bookingCreateInput)=> {
-//   try{
+const createBooking = async (data: Prisma.bookingCreateInput)=> {
+  try{ const result = await prisma.booking.create({ //orm object relation model
+    data,
     
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// }
+  }); return result
+ } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export const bookingDao = { createBooking }
