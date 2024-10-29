5a2a0016c2b04fbefc7ad0c99a6a5c17e4fce116
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-10-29 23:57:55 +0500

    adds getUser and updateUser dao to userDao

--- src/dao/user.ts
+++ src/dao/user.ts
@@ -1,16 +1,48 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 
-
-const createUser = async (prisma: PrismaClient, data: Prisma.userCreateInput) => {
+const createUser = async (
+  prisma: PrismaClient,
+  data: Prisma.userCreateInput
+) => {
   try {
-    const result = await prisma.user.create({ //orm object relation model
+    const result = await prisma.user.create({
       data,
     });
-    return result
+    return result;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
 
-export const userDao = { createUser }
+
+const getUser = async (prisma: PrismaClient, id: number) => {
+  try {
+    const result = await prisma.user.findUnique({
+      where: { id },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateUser = async (
+  prisma: PrismaClient,
+  id: number,
+  data: Prisma.userUpdateInput
+) => {
+  try {
+    const result = await prisma.user.update({
+      where: { id },
+      data,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const userDao = { createUser, getUser, updateUser };
