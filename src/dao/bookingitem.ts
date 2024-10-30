3c417faf4d638ba9ecf6a70ef61b34729b504119
Abdul Qayoom <qayoomk013@gmail.com>
2024-10-30 21:10:36 +0500

    feature#24-UPDATE-GET-bookingitem-dao

--- src/dao/bookingitem.ts
+++ src/dao/bookingitem.ts
@@ -13,4 +13,34 @@ const createBookingitem = async (prisma: PrismaClient, data: Prisma.booking_item
   }
 };
 
-export const booking_itemDao = { createBookingitem }
+const getBookingitem = async (prisma: PrismaClient, id: number) => {
+  try {
+    const result = await prisma.booking_item.findUnique({
+      where: { id },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateBookingitem = async (
+  prisma: PrismaClient,
+  id: number,
+  data: Prisma.booking_itemUpdateInput
+) => {
+  try {
+    const result = await prisma.booking_item.update({
+      where: { id },
+      data,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const bookingitemDao = { createBookingitem, getBookingitem, updateBookingitem };
+
