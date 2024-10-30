361b53d85802eb8188e99fa574baef5131f3802e
Abdul Qayoom <qayoomk013@gmail.com>
2024-10-30 21:33:02 +0500

    feature#27-UPDATE-GET-service-dao

--- src/dao/service.ts
+++ src/dao/service.ts
@@ -13,4 +13,34 @@ const createService = async (prisma: PrismaClient, data: Prisma.serviceCreateInp
   }
 };
 
-export const serviceDao = { createService }
+const getService = async (prisma: PrismaClient, id: number) => {
+  try {
+    const result = await prisma.service.findUnique({
+      where: { id },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateService = async (
+  prisma: PrismaClient,
+  id: number,
+  data: Prisma.serviceUpdateInput
+) => {
+  try {
+    const result = await prisma.service.update({
+      where: { id },
+      data,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const userDao = { createService, getService, updateService };
+
