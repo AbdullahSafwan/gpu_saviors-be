879d74a039e44fe65989d49902b464265dcf7660
Abdul Qayoom <qayoomk013@gmail.com>
2024-10-30 21:20:03 +0500

    Feature/25-UPDATE-GET-booking-payment-dao

--- src/dao/bookingpayment.ts
+++ src/dao/bookingpayment.ts
@@ -13,4 +13,34 @@ const createBookingpayment = async (prisma: PrismaClient, data: Prisma.booking_p
   }
 };
 
-export const booking_paymentDao = { createBookingpayment }
+const getBookingpaymet = async (prisma: PrismaClient, id: number) => {
+  try {
+    const result = await prisma.booking_payment.findUnique({
+      where: { id },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateBookingpayment = async (
+  prisma: PrismaClient,
+  id: number,
+  data: Prisma.booking_paymentUpdateInput
+) => {
+  try {
+    const result = await prisma.booking_payment.update({
+      where: { id },
+      data,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const userDao = { createBookingpayment, getBookingpaymet, updateBookingpayment };
+
