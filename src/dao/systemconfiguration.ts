20c85671d5c6d0675012144341929a4cc706789f
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-01 02:26:14 +0500

    feature#21-update-get-sys-config-dao

--- src/dao/systemconfiguration.ts
+++ src/dao/systemconfiguration.ts
@@ -15,4 +15,33 @@ const createSystemconfiguration = async (
   }
 };
 
-export const system_configurationDao = { createSystemconfiguration };
+const getSystemconfiguration = async (prisma: PrismaClient, id: number) => {
+  try {
+    const result = await prisma.system_configuration.findUnique({
+      where: { id },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const updateSystemconfiguration = async (
+  prisma: PrismaClient,
+  id: number,
+  data: Prisma.system_configurationUpdateInput
+) => {
+  try {
+    const result = await prisma.system_configuration.update({
+      where: { id },
+      data,
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const system_configurationDao = { createSystemconfiguration, getSystemconfiguration, updateSystemconfiguration };
