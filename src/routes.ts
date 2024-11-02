540714dd1e3dea10d7c8cd5854070b3e69662a12
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-02 20:52:59 +0500

    feature/49 create crud operation delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getController } from './controllers';
 import { userController } from './controllers/user';
 import { userValidator } from './middleware/validator/userValidator';
+import { deliveryController } from './controllers/delivery';
 // import { throwValidationResult } from './services/helper';
 
 const router = express.Router();
@@ -10,5 +11,8 @@ router.get('/', getController)
 router.post('/user/',userValidator.userCreateValidator, userController.createUser)
 router.get('/user/:id', userController.getUserDetails)
 
+router.post('/delivery/',deliveryController.createDelivery)
+router.get('/delivery/:id',deliveryController.getDeliveryDetails)
+router.patch('/delivery/:id',deliveryController.updateDelivery)
 
 export default router;
