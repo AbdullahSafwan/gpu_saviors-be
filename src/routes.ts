847a1dd41af65e9af0347b42b48b9d60590eaf4a
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-02 21:21:12 +0500

    feature/51 create crud operation service

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getController } from './controllers';
 import { userController } from './controllers/user';
 import { userValidator } from './middleware/validator/userValidator';
+import { serviceController } from './controllers/service';
 // import { throwValidationResult } from './services/helper';
 
 const router = express.Router();
@@ -9,6 +10,10 @@ const router = express.Router();
 router.get('/', getController)
 router.post('/user/',userValidator.userCreateValidator, userController.createUser)
 router.get('/user/:id', userController.getUserDetails)
+
+router.get('/user/',serviceController.createService)
+router.post('/service/:id', serviceController.getServiceDetails)
+router.get('/service/:id',serviceController.updateService)
 
 
 export default router;
