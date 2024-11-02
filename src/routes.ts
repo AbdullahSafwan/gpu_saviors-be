052d0b91236343a49ea951c64f46229d0d6db5ff
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-02 21:08:32 +0500

    feature/50 creates crud operation refund

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getController } from './controllers';
 import { userController } from './controllers/user';
 import { userValidator } from './middleware/validator/userValidator';
+import { refundController } from './controllers/refund';
 // import { throwValidationResult } from './services/helper';
 
 const router = express.Router();
@@ -9,6 +10,11 @@ const router = express.Router();
 router.get('/', getController)
 router.post('/user/',userValidator.userCreateValidator, userController.createUser)
 router.get('/user/:id', userController.getUserDetails)
+
+
+router.post('/refund/',refundController.createRefund)
+router.get('/refund/:id',refundController.getRefundDetails)
+router.patch('/refund/:id',refundController.updateRefund)
 
 
 export default router;
