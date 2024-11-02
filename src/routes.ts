4f07f327697b6c9e141763c833a8e43321923b7b
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-02 15:35:42 +0500

    feature/44-create-cred-operation-user

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
-import express from 'express';
-import { getController } from './controllers';
-import { userController } from './controllers/user';
-import { userValidator } from './middleware/validator/userValidator';
+import express from "express";
+import { getController } from "./controllers";
+import { userController } from "./controllers/user";
+import { userValidator } from "./middleware/validator/userValidator";
 // import { throwValidationResult } from './services/helper';
 
 const router = express.Router();
 
-router.get('/', getController)
-router.post('/user/',userValidator.userCreateValidator, userController.createUser)
-router.get('/user/:id', userController.getUserDetails)
-
+router.get("/", getController);
+router.post(
+  "/user/",
+  userValidator.userCreateValidator,
+  userController.createUser
+);
+router.get("/user/:id", userController.getUserDetails);
+router.patch("/user/:id", userController.updateUser);
 
 export default router;
