331b0277d69ac67275401683d553017d27abd07e
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-02 21:38:04 +0500

    feature/52 create crud operaion system_configuration

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { getController } from './controllers';
 import { userController } from './controllers/user';
 import { userValidator } from './middleware/validator/userValidator';
+import { system_configurationController } from './controllers/systemConfiguration';
 // import { throwValidationResult } from './services/helper';
 
 const router = express.Router();
@@ -9,6 +10,11 @@ const router = express.Router();
 router.get('/', getController)
 router.post('/user/',userValidator.userCreateValidator, userController.createUser)
 router.get('/user/:id', userController.getUserDetails)
+
+
+router.post('/systemConfiguration/',system_configurationController.createSystemConfiguration)
+router.get('/systemConfiguration/:key',system_configurationController.getSystemConfigurationDetails)
+router.patch('/systemConfiguration/:key',system_configurationController.updateSystemConfiguration)
 
 
 export default router;
