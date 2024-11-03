8b89b31f1319a763a8060708d55a0f4cc1a18aac
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-11-03 14:28:58 +0500

    adds validation error helper function

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,18 @@
-import express from 'express';
-import { getController } from './controllers';
-import { userController } from './controllers/user';
-import { userValidator } from './middleware/validator/userValidator';
-// import { throwValidationResult } from './services/helper';
+import express from "express";
+import { getController } from "./controllers";
+import { userController } from "./controllers/user";
+import { userValidator } from "./middleware/validator/userValidator";
+import { throwValidationResult } from "./services/helper";
 
 const router = express.Router();
 
-router.get('/', getController)
-router.post('/user/',userValidator.userCreateValidator, userController.createUser)
-router.get('/user/:id', userController.getUserDetails)
-
+router.get("/", getController);
+router.post(
+  "/user/",
+  userValidator.userCreateValidator,
+  throwValidationResult,
+  userController.createUser
+);
+router.get("/user/:id", userController.getUserDetails);
 
 export default router;
