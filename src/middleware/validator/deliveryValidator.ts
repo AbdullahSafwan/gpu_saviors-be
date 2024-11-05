90b75e35b1b9094a1a56de2c7e345f93c328c063
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-05 20:33:06 +0500

    feature/59 create crud validation delivery

---
+++ src/middleware/validator/deliveryValidator.ts
@@ -0,0 +1,17 @@
+import { body } from "express-validator";
+
+const deliveryCreateValidator = [
+  body("address").notEmpty().withMessage("address is required"),
+
+  body("phoneNumber")
+    .notEmpty()
+    .isMobilePhone("any")
+    .withMessage("Invalid Phone Number"),
+
+    body("landmark").optional().withMessage("landmark is optional"),
+
+    body("secondaryPhoneNumber").optional().notEmpty().withMessage("Secondary PhoneNumber is optional")
+
+];
+
+export const deliveryValidator = { deliveryCreateValidator };
