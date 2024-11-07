8c11513871c58fb0143666433adc250dd47d30b8
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-11-07 23:36:19 +0500

    booking payableAmount using bookingItems

--- src/controllers/booking.ts
+++ src/controllers/booking.ts
@@ -1,56 +1,53 @@
 import { Request, Response } from "express";
 import { bookingDao } from "../dao/booking";
 import prisma from "../prisma";
-// import { booking_item } from "@prisma/client";
+import { booking_item } from "@prisma/client";
 
 const createBooking = async (req: Request, res: Response) => {
-    try {
-
+  try {
     const data = req.body;
+    //calculating booking payableAmount using sum of all bookingItem payableAmount
+    data.payableAmount = data.booking_items.reduce(
+      (total: number, item: booking_item) => total + item.payableAmount,
+      0
+    );
 
-    // let bookingItem: booking_item[] = []
-    data.booking_item = {
-      create: data.booking_item
-    }
-    const result = await bookingDao.createBooking (prisma, data);
+    data.booking_items = {
+      create: data.booking_items,
+    };
+
+    const result = await bookingDao.createBooking(prisma, data);
     res.status(200).send(result);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
-}
-
-}
-
-
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
 
 const getBookingDetails = async (req: Request, res: Response) => {
-    try {
-      const id = req.params.id ? +req.params?.id : null;
-      if (!id) {
-        throw Error("id is required");
-      }
-      const result = await bookingDao.getBooking(prisma, id);
-      res.status(200).send(result);
-    } catch (error) {
-      console.log(error);
-      res.status(400).send(error);
+  try {
+    const id = req.params.id ? +req.params?.id : null;
+    if (!id) {
+      throw Error("id is required");
     }
-  };
-
-
-
-  const updateBooking = async (req:Request, res: Response) => {
-    try {
-        const data = req.body
-        const id =+req.params.id
-        const result = await bookingDao.updateBooking(prisma,id,data);
-        res.status(200).send(result);
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
-    }
+    const result = await bookingDao.getBooking(prisma, id);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
   }
+};
 
-  export const bookingController = {createBooking,getBookingDetails,updateBooking}
+const updateBooking = async (req: Request, res: Response) => {
+  try {
+    const data = req.body;
+    const id = +req.params.id;
+    const result = await bookingDao.updateBooking(prisma, id, data);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+};
+
+export const bookingController = { createBooking, getBookingDetails, updateBooking };
