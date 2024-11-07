7ee9cfa44192c501f1d5bfa3ec809f787888c320
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-07 09:57:55 +0500

    feature/59 create deliveryUpdateValidator for delivery

--- src/middleware/validator/deliveryValidator.ts
+++ src/middleware/validator/deliveryValidator.ts
@@ -10,8 +10,39 @@ const deliveryCreateValidator = [
 
  body("landmark").optional().withMessage("landmark is optional"),
 
-body("secondaryPhoneNumber").optional().notEmpty().withMessage("Secondary PhoneNumber is optional")
+ body("secondaryPhoneNumber").optional().notEmpty().withMessage("Secondary PhoneNumber is optional"),
+
+ body("bookingId").isInt().notEmpty().withMessage("BookingId is required"),
+
+ body("status").notEmpty().withMessage("status is req"),
+
+ body ("deliveryDate").isInt().notEmpty().withMessage("DateTime is required")
 
 ];
 
-export const deliveryValidator = { deliveryCreateValidator };
+const deliveryUpdateValidator = [
+
+  body("id").isInt().notEmpty().withMessage("id is required"),
+
+  body("address").optional().notEmpty().withMessage("address is req"),
+
+    body("phoneNumber").optional()
+    .notEmpty()
+    .isMobilePhone("any")
+    .withMessage("Invalid Phone Number"),
+
+ body("landmark").optional().withMessage("landmark is optional"),
+
+ body("secondaryPhoneNumber").optional().notEmpty().withMessage("Secondary PhoneNumber is optional"),
+
+ body("bookingId").isInt().optional().notEmpty().withMessage("BookingId is required"),
+
+ body("status").optional().notEmpty().withMessage("status is req"),
+
+ body ("deliveryDate").optional().isInt().notEmpty().withMessage("DateTime is required")
+
+
+];
+
+
+export const deliveryValidator = { deliveryCreateValidator,deliveryUpdateValidator };
