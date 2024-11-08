88dedf0d3a4f306dc838c94da351319518e73442
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-11-08 22:34:05 +0500

    plugin security

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import pluginSecurity from "eslint-plugin-security";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    extends: [pluginSecurity.configs.recommended],
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
