6a8a6457758addb08529a8f7fa786332845cda44
Abdul Qayoom <qayoomk013@gmail.com>
2024-11-11 09:25:04 +0500

    adds deliveryUpdateValidator and removes unnecessary route APIs

--- src/middleware/validator/deliveryValidator.ts
+++ src/middleware/validator/deliveryValidator.ts
@@ -1,4 +1,4 @@
-import { delivery_status } from "@prisma/client";
+import { courier_type, delivery_status } from "@prisma/client";
 import { body } from "express-validator";
 
 const createDeliveryValidator = [
@@ -15,6 +15,12 @@ const createDeliveryValidator = [
   body("status").notEmpty().withMessage("status is req").bail().isIn(Object.values(delivery_status)),
 
   body("deliveryDate").notEmpty().withMessage("DateTime is required").isISO8601().toDate(),
+
+  body ("postalCode").notEmpty().withMessage("Postal code is required").isInt(),
+
+  body ("courier").notEmpty().isString().withMessage("Courier name is required"),
+
+  body ("type").notEmpty().withMessage("courier type is required").bail().isIn(Object.values(courier_type))
 ];
 
 const updateDeliveryValidator = [
@@ -30,9 +36,15 @@ const updateDeliveryValidator = [
 
   body("bookingId").isInt().optional().notEmpty().withMessage("BookingId is required"),
 
-  body("status").optional().notEmpty().withMessage("status is req"),
+  body("status").optional().notEmpty().withMessage("status is req").bail().isIn(Object.values(delivery_status)),
 
   body("deliveryDate").optional().notEmpty().withMessage("DateTime is required").isISO8601().toDate(),
+
+  body ("postalCode").optional().notEmpty().withMessage("Postal code is required").isInt(),
+
+  body ("courier").optional().notEmpty().isString().withMessage("Courier name is required"),
+
+  body ("type").optional().notEmpty().withMessage("courier type is required").bail().isIn(Object.values(courier_type))
 ];
 
 export const deliveryValidator = { createDeliveryValidator, updateDeliveryValidator };
