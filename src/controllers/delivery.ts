9ab4749dcf876b9ac0577ff359a520c75e261ae3
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-11-20 22:42:04 +0500

    adds delivery service

--- src/controllers/delivery.ts
+++ src/controllers/delivery.ts
@@ -1,27 +1,20 @@
 import { Request, Response } from "express";
-import { deliveryDao } from "../dao/delivery";
-import prisma from "../prisma";
 import { CreateDeliveryRequest, UpdateDeliveryRequest } from "../types/deliveryTypes";
 import { debugLog } from "../services/helper";
 import { sendSuccessResponse, sendErrorResponse } from "../services/responseHelper";
+import { deliveryService } from "../services/delivery";
 
 const createDelivery = async (req: Request<{}, {}, CreateDeliveryRequest>, res: Response) => {
   try {
-    const { bookingId, ...rest } = req.body; // Destructure to exclude `bookingId`
+    const data = req.body;
 
-    // Construct deliveryData, converting `bookingId` to a nested connect object
-    const deliveryData = {
-      ...rest,
-      booking: { connect: { id: bookingId } },
-    };
-
-    const result = await deliveryDao.createDelivery(prisma, deliveryData);
+    const result = await deliveryService.createDelivery(data);
     sendSuccessResponse(res, 200, "Successfully created delivery", result);
   } catch (error) {
     debugLog(error);
     sendErrorResponse(res, 400, "Error creating delivery", error);
   }
-}; 
+};
 
 const getDeliveryDetails = async (req: Request<{ id: string }>, res: Response) => {
   try {
@@ -29,23 +22,19 @@ const getDeliveryDetails = async (req: Request<{ id: string }>, res: Response) =
     if (!id) {
       throw new Error("id is required");
     }
-    const result = await deliveryDao.getDelivery(prisma, id);
-    if (!result) {
-      throw new Error(`delivery not found against id: ${id}`);
-    }
+    const result = await deliveryService.getDelivery(id);
     sendSuccessResponse(res, 200, "Successfully fetched delivery", result);
   } catch (error) {
     debugLog(error);
     sendErrorResponse(res, 400, "Error fetching delivery", error);
   }
-  
 };
 
 const updateDelivery = async (req: Request<{ id: string }, {}, UpdateDeliveryRequest>, res: Response) => {
   try {
     const data = req.body;
     const id = +req.params.id;
-    const result = await deliveryDao.updateDelivery(prisma, id, data);
+    const result = await deliveryService.updateDelivery(id, data);
     sendSuccessResponse(res, 200, "Successfully updated delivery", result);
   } catch (error) {
     debugLog(error);
---
+++ src/services/delivery/index.ts
@@ -0,0 +1,47 @@
+import prisma from "../../prisma";
+import { debugLog } from "../helper";
+import { CreateDeliveryRequest, UpdateDeliveryRequest } from "../../types/deliveryTypes";
+import { deliveryDao } from "../../dao/delivery";
+
+const createDelivery = async (data: CreateDeliveryRequest) => {
+  try {
+    const { bookingId, ...rest } = data; // Destructure to exclude `bookingId`
+
+    // Construct deliveryData, converting `bookingId` to a nested connect object
+    const deliveryData = {
+      ...rest,
+      booking: { connect: { id: bookingId } },
+    };
+
+    const result = await deliveryDao.createDelivery(prisma, deliveryData);
+
+    return result;
+  } catch (error) {
+    debugLog(error);
+    throw error;
+  }
+};
+
+const getDelivery = async (id: number) => {
+  try {
+    const result = await deliveryDao.getDelivery(prisma, id);
+    if (!result) {
+      throw new Error(`delivery not found against id: ${id}`);
+    }
+    return result;
+  } catch (error) {
+    debugLog(error);
+    throw error;
+  }
+};
+
+const updateDelivery = async (id: number, data: UpdateDeliveryRequest) => {
+  try {
+    const result = await deliveryDao.updateDelivery(prisma, id, data);
+    return result;
+  } catch (error) {
+    debugLog(error);
+    throw error;
+  }
+};
+export const deliveryService = { createDelivery, getDelivery, updateDelivery };
