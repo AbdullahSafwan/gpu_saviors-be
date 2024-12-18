6bfbf45daf205fc8acd01288228c5f9f8eea7a8b
Abdul Qayoom <qayoomk013@gmail.com>
2024-12-18 14:42:29 +0500

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import app from "./app";
+import swaggerDocs from "./swagger";
 
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 console.log({ port });
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
+
+  swaggerDocs(app, port)
 });
+
+export default app
---
+++ src/swagger.ts
@@ -0,0 +1,54 @@
+import { Express,Request,Response } from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { version } from "../package.json";
+// import app from "./app";
+// import port from "../src/index"
+
+
+const option : swaggerJsdoc.Options = {
+definition: {
+    openapi: "3.0.0",
+    info:{ 
+        title: "REST API Docs",
+        version
+    },
+    components:{
+        securitySchemas:{
+            bearerAuth:{
+                type: "http",
+                scheme: "bearer",
+                bearerFormat: "JWT"
+            },
+        },
+    },
+    security: [
+        {
+            bearerAuth: []
+        },
+    ],
+
+},
+apis: ["./route.ts", "../prisma/*.ts"]
+};
+
+
+const swaggerSpec = swaggerJsdoc (option)
+
+function swaggerDocs (app: Express,port: number){
+// swagger page 
+
+app.use("/docs",swaggerUi.serve,swaggerUi.setup (swaggerSpec));
+
+
+// Docs in JSON Format 
+
+app.get("docs.json",(_req:Request, res:Response) =>{
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec)
+});
+
+console.log(`Docs available at http://localhost:${port}/docs`);
+}
+
+export default swaggerDocs;
