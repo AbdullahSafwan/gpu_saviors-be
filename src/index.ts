c3778d7ee88ee2d0f31f7d675917bf0eb3f3228f
AbdullahSafwan <abdullahsafwan768@gmail.com>
2024-12-29 15:55:10 +0500

    changes data type conversion method for server port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import swaggerDocs from "./swagger";
 
-const port = Number(process.env.PORT) || 8080;
+const port = process.env.PORT ? +process.env.PORT : 8080;
 console.log({ port });
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
